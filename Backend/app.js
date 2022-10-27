d2a9c59849fbe5b43751be1a39040e139aa31476
reshmatheporal <reshmatheporal@gmail.com>
2022-10-27 16:50:02 +0530

    secondpush

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express()
-app.use(express.json())
-app.use(router)
-mongoose.connect(config.URL)
-.then(()=>console.log('connected to mongose..'))
-.catch(err=>console.log('could not connect to mongoDB...',err))
-port=config.PORT;
-app.listen(port,()=>{
-    console.log(`Lisening the sever ${port}....`)
-})
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/config.json");
+const router = require("./src/router/router");
+const logger = require("./logger");
+
+const app = express();
+app.use(express.json());
+app.use(router);
+mongoose
+  .connect(config.URL)
+  .then(() => console.log("connected to mongose.."))
+  .catch((err) => console.log("could not connect to mongoDB...", err));
+port = config.PORT;
+app.listen(port, () => {
+  console.log(`Lisening the sever ${port}....`);
+});
