888cfe0d234e9f1930bc076a2b8557219e8958df
sbharathram23 <sbharathram23@gmailcom>
2022-10-28 16:40:16 +0530

    i completed manufACTUTER details

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Registration from "./components/Registration";
-
+import Home from "./components/home";
 const Routering = () => {
   return (
     <div >
     <Router>
       <Routes>
         <Route exact path="/" element={<Registration />} />
-        
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </Router>
     </div>
